c2e560d5252cb6510c869a1bb269ac227c678708
gabs1311 <gabs.antonovici@gmail.com>
2022-05-09 22:17:47 +0300

    day 2 T1 : CydeoVerification

--- src/test/java/com/cydeo/tests/day2_locator_getText_getAttribute/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day2_locator_getText_getAttribute/T1_CydeoVerifications.java
@@ -16,12 +16,28 @@ public class T1_CydeoVerifications {
 
 
         //2. Go to https://practice.cydeo.com
+
+        driver.navigate().to("https://practice.cydeo.com");
         //3. Verify URL contains
         //Expected: cydeo
+
+        String expectedURL="cydeo";
+        String actualURL = driver.getCurrentUrl();
+        if (actualURL.equals(expectedURL)){
+            System.out.println("URL verification passed!");
+        }else {
+            System.out.println("URL verification failed!!!");}
+
         //4. Verify title:
         //Expected: Practice
+        String expectedTitle="Practice";
+        String actualTitle = driver.getTitle();
+        if (actualTitle.equals(expectedTitle)){
+            System.out.println(" Title verification PASSED");
+        }else{
+            System.out.println(" Title verification FAILED");}
 
-
+        driver.close();
 
 
 
