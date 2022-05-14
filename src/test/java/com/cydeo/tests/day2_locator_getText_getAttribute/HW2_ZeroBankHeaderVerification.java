9d9108294a6f11b378d5fe9c09543df215b90f49
gabs1311 <gabs.antonovici@gmail.com>
2022-05-14 19:08:52 +0300

    day 2 HW2 Zero Bank Header Verification

---
+++ src/test/java/com/cydeo/tests/day2_locator_getText_getAttribute/HW2_ZeroBankHeaderVerification.java
@@ -0,0 +1,21 @@
+package com.cydeo.tests.day2_locator_getText_getAttribute;
+
+public class HW2_ZeroBankHeaderVerification {
+    public static void main(String[] args) {
+
+        /*
+        TC #2: Zero Bank header verification
+1. Open Chrome browser
+2. Go to http://zero.webappsecurity.com/login.html
+3. Verify header text
+Expected: “Log in to ZeroBank
+         */
+
+
+        //TC #2: Zero Bank header verification
+        //1. Open Chrome browser
+        //2. Go to http://zero.webappsecurity.com/login.html
+        //3. Verify header text
+        //Expected: “Log in to ZeroBank
+    }
+}
