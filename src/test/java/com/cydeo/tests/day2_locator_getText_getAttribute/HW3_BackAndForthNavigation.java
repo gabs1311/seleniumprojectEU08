f6bb267ecc78b993391dfc0f86f91a42c8b33930
gabs1311 <gabs.antonovici@gmail.com>
2022-05-14 19:09:09 +0300

    day 2 HW3 Back and Forth Navigation

---
+++ src/test/java/com/cydeo/tests/day2_locator_getText_getAttribute/HW3_BackAndForthNavigation.java
@@ -0,0 +1,27 @@
+package com.cydeo.tests.day2_locator_getText_getAttribute;
+
+public class HW3_BackAndForthNavigation {
+    public static void main(String[] args) {
+        /*
+        TC #3: Back and forth navigation
+1- Open a chrome browser
+2- Go to: https://google.com
+3- Click to Gmail from top right.
+4- Verify title contains:
+Expected: Gmail
+5- Go back to Google by using the .back();
+6- Verify title equals:
+Expected: Google
+         */
+
+        //TC #3: Back and forth navigation
+        //1- Open a chrome browser
+        //2- Go to: https://google.com
+        //3- Click to Gmail from top right.
+        //4- Verify title contains:
+        //Expected: Gmail
+        //5- Go back to Google by using the .back();
+        //6- Verify title equals:
+        //Expected: Google
+    }
+}
