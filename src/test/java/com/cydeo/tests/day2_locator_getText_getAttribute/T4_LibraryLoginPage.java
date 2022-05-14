50031860e2f54d732371cebabbe11df6175951e3
gabs1311 <gabs.antonovici@gmail.com>
2022-05-14 13:00:55 +0300

    day 2 T4: library login page

--- src/test/java/com/cydeo/tests/day2_locator_getText_getAttribute/T4_LibraryLoginPage.java
+++ src/test/java/com/cydeo/tests/day2_locator_getText_getAttribute/T4_LibraryLoginPage.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day2_locator_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T4_LibraryLoginPage {
@@ -34,9 +36,16 @@ Locate Sign in button using “tagName” locator
         driver.get(" https://library2.cybertekschool.com/login.html ");
 
 //3. Enter username: “incorrect@email.com”
+        WebElement usernameInput= driver.findElement(By.className("form-control"));
+   usernameInput.sendKeys("incorrect@email.com");
 
 //4. Enter password: “incorrect password”
+        WebElement passwordInput= driver.findElement(By.id("inputPassword"));
+        passwordInput.sendKeys("incorrect password");
+
         //5. Click to sign in button
+        WebElement signInbutton = driver.findElement(By.name("button"));
+
 //6. Verify: visually “Sorry, Wrong Email or Password”
 //displayed
 //PS: Locate username input box using “className” locator
