aa8db7025106554418970df266d7e8ff145f37e6
gabs1311 <gabs.antonovici@gmail.com>
2022-05-14 16:30:51 +0300

    day 2 T5: getText, getAttribute

--- src/test/java/com/cydeo/tests/day2_locator_getText_getAttribute/T5_GetTextGetAttribute.java
+++ src/test/java/com/cydeo/tests/day2_locator_getText_getAttribute/T5_GetTextGetAttribute.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day2_locator_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T5_GetTextGetAttribute {
@@ -26,8 +28,19 @@ Expected: first name
         driver.manage().window().maximize();
         //2- Go to: https://practice.cydeo.com/registration_form
         driver.get("https://practice.cydeo.com/registration_form");
+
         //3- Verify header text is as expected:
+        WebElement headerText = driver.findElement(By.tagName("h2"));
         //Expected: Registration form
+        String expectedHeaderText="Registration form";
+        String actualHeaderText = headerText.getText();// wil return "Registration form" as String
+
+
+        if (actualHeaderText.equals(expectedHeaderText)) {
+            System.out.println("Header text verification PASSED");
+        }else{
+            System.out.println("Headertext verification FAILED");
+        }
         //4- Locate “First name” input box
         //5- Verify placeholder attribute’s value is as expected:
         //Expected: first name
