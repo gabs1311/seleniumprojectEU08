beb91064663611395baa1eaf826f35f214174a46
gabs1311 <gabs.antonovici@gmail.com>
2022-05-14 19:09:29 +0300

    day 2 HW4 Paractice Cydeo CLass Locator

---
+++ src/test/java/com/cydeo/tests/day2_locator_getText_getAttribute/HW4_PractticeCydeo_ClassLocatorPractice.java
@@ -0,0 +1,24 @@
+package com.cydeo.tests.day2_locator_getText_getAttribute;
+
+public class HW4_PractticeCydeo_ClassLocatorPractice {
+    public static void main(String[] args) {
+        /*
+        TC #4: Practice Cydeo – Class locator practice
+1- Open a chrome browser
+2- Go to: https://practice.cydeo.com/inputs
+3- Click to “Home” link
+4- Verify title is as expected:
+Expected: Practice
+PS: Locate “Home” link using “className” locator
+         */
+
+        //TC #4: Practice Cydeo – Class locator practice
+        //1- Open a chrome browser
+        //2- Go to: https://practice.cydeo.com/inputs
+        //3- Click to “Home” link
+        //4- Verify title is as expected:
+        //Expected: Practice
+        //PS: Locate “Home” link using “className” locator
+
+    }
+}
