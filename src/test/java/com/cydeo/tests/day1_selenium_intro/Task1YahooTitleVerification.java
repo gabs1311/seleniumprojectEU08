27f98e643cb480e31d2fa5aea1a5af1c8075307f
gabs1311 <gabs.antonovici@gmail.com>
2022-05-14 12:30:52 +0300

    day 2 T4: library login page

--- src/test/java/com/cydeo/tests/day1_selenium_intro/Task1YahooTitleVerification.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/Task1YahooTitleVerification.java
@@ -1,10 +1,15 @@
 package com.cydeo.tests.day1_selenium_intro;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Task1YahooTitleVerification {
+
+    private static Object consent;
+
 
     public static void main(String[] args) {
 
@@ -23,10 +28,19 @@ public class Task1YahooTitleVerification {
 
 
         //2. Go to https://www.yahoo.com
-        driver.get("https://www.yahoo.com");
-        //3. Verify title:
+        driver.get("https://www.yahoo.com/%22");
+
+        WebElement acceptButton= driver.findElement(By.tagName("button"));
+        acceptButton.click();
+
+       // WebElement clickOnUsingButton=driver.findElement(By.xpath("//*[@id="consent-page"]/div/div/div/form/div[2]/div[2]/button"));
+
+      //  clickOnUsingButton.click();
+
+
+
         //Expected: Yahoo
-        String expectedTitle = "Yahoo";
+        String expectedTitle = "Yahoo|Mail,Stiri,Sporturi,Mobile,Vreme,Mai multe";
 
         // actual title comes from the browser
         String actualTitle = driver.getTitle();
--- src/test/java/com/cydeo/tests/day2_locator_getText_getAttribute/T3_GoogleSearch.java
+++ src/test/java/com/cydeo/tests/day2_locator_getText_getAttribute/T3_GoogleSearch.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day2_locator_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class T3_GoogleSearch {
@@ -25,13 +28,30 @@ Expected: Title should start with “apple” word
         driver.manage().window().maximize();
 
         //2- Go to: https://google.com
-        driver.get(" https://google.com ");
-        //3- Write “apple” in search box
+        driver.get(" https://google.com");
 
+
+
+        //3- Write “apple” in search box
         //4- Click google search button
+
+       WebElement googleSearchBox= driver.findElement(By.name("q"));
+
+       googleSearchBox.sendKeys("apple"+ Keys.ENTER);
+
+// this does not go past google.com Sunt de acord page. Need to fix it
+
+
         //5- Verify title:
         //Expected: Title should start with “apple” word
+        String expectedInTitle = "apple";
+        String actualTitle = driver.getTitle();
 
+        if (actualTitle.startsWith(expectedInTitle)){
+            System.out.println(" Title verification PASSED");
+        }else{
+            System.out.println("Title verification FAILED");
+        }
 
     }
 }
