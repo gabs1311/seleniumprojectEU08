bc015bc7f896a9a87b1fa1699f2bd04a3e076057
gabs1311 <gabs.antonovici@gmail.com>
2022-05-15 15:59:37 +0300

    day 3 T1

---
+++ src/test/java/com/cydeo/tests/day3_cssSelecterAndExpath/T1_NextBaseCRM_locatorsAndGetText_practice.java
@@ -0,0 +1,32 @@
+package com.cydeo.tests.day3_cssSelecterAndExpath;
+
+public class T1_NextBaseCRM_locatorsAndGetText_practice {
+    public static void main(String[] args) {
+
+
+
+        /*TC #1: NextBaseCRM, locators and getText() practice
+1- Open a chrome browser
+2- Go to: https://login1.nextbasecrm.com/
+3- Enter incorrect username: “incorrect”
+4- Enter incorrect password: “incorrect”
+5- Click to login button.
+6- Verify error message text is as expected:
+Expected: Incorrect login or password
+PS: Inspect and decide which locator you should be using to locate web
+elements.
+
+         */
+
+        //TC #1: NextBaseCRM, locators and getText() practice
+        //1- Open a chrome browser
+        //2- Go to: https://login1.nextbasecrm.com/
+        //3- Enter incorrect username: “incorrect”
+        //4- Enter incorrect password: “incorrect”
+        //5- Click to login button.
+        //6- Verify error message text is as expected:
+        //Expected: Incorrect login or password
+        //PS: Inspect and decide which locator you should be using to locate web
+        //elements.
+    }
+}
