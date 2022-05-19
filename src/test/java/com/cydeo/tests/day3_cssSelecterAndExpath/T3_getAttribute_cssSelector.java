ab8637309991816b74c561d01b6bc7c80335ce2d
gabs1311 <gabs.antonovici@gmail.com>
2022-05-19 12:34:37 +0300

    day3 T3  getAttribute_cssSelector

--- src/test/java/com/cydeo/tests/day3_cssSelecterAndExpath/T3_getAttribute_cssSelector.java
+++ src/test/java/com/cydeo/tests/day3_cssSelecterAndExpath/T3_getAttribute_cssSelector.java
@@ -1,5 +1,8 @@
 package com.cydeo.tests.day3_cssSelecterAndExpath;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
 public class T3_getAttribute_cssSelector {
 
     /*
@@ -17,9 +20,15 @@ PS2: Pay attention to where to get the text of this button from
     public static void main(String[] args) {
         //TC #3: NextBaseCRM, locators, getText(), getAttribute() practice
         //1- Open a chrome browser
+    WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
         //2- Go to: https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
         //3- Verify “Log in” button text is as expected:
         //Expected: Log In
+
         //PS: Inspect and decide which locator you should be using to locate web
         //elements.
         //PS2: Pay attention to where to get the text of this button from
