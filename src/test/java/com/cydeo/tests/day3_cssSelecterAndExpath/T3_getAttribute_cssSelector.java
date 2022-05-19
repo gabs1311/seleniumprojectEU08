13bc346f1f9d1873c69f837efe080fc53b60240f
gabs1311 <gabs.antonovici@gmail.com>
2022-05-19 13:17:17 +0300

    day3 T3  getAttribute_cssSelector

--- src/test/java/com/cydeo/tests/day3_cssSelecterAndExpath/T3_getAttribute_cssSelector.java
+++ src/test/java/com/cydeo/tests/day3_cssSelecterAndExpath/T3_getAttribute_cssSelector.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day3_cssSelecterAndExpath;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class T3_getAttribute_cssSelector {
 
@@ -20,7 +22,7 @@ PS2: Pay attention to where to get the text of this button from
     public static void main(String[] args) {
         //TC #3: NextBaseCRM, locators, getText(), getAttribute() practice
         //1- Open a chrome browser
-    WebDriver driver = WebDriverFactory.getDriver("chrome");
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
 
         //2- Go to: https://login1.nextbasecrm.com/
@@ -28,9 +30,29 @@ PS2: Pay attention to where to get the text of this button from
 
         //3- Verify “Log in” button text is as expected:
         //Expected: Log In
+        //  WebElement signInButton= driver.findElement(By.className("login-btn"));
 
-        //PS: Inspect and decide which locator you should be using to locate web
-        //elements.
-        //PS2: Pay attention to where to get the text of this button from
+        //LOCATING THE SAME WEB ELEMENT USING DIFFERENT ATTRIBUTE VALUES.
+        //	                   tagName[attribute='value']
+        //	                   input[class='login-btn']
+        // LOCATED USING CLASS ATTRIBUTE
+        //WebElement signInButton = driver.findElement(By.cssSelector("input[class='login-btn']"));
+        // LOCATED USING TYPE ATTRIBUTE
+        //WebElement signInButton = driver.findElement(By.cssSelector("input[type='submit']"));
+        // LOCATED USING VALUE ATTRIBUTE
+        WebElement signInButton = driver.findElement(By.cssSelector("input[value='Log In']"));
+
+        String expectedButtonText = "Log In";
+
+        //Getting the value of the attribute "value"
+        String actualButtonText = signInButton.getAttribute("value");
+
+        System.out.println("actualButtonText = " + actualButtonText);
+
+        if (actualButtonText.equals(expectedButtonText)) {
+            System.out.println("Log In button text verification passed!");
+        } else {
+            System.out.println("Log In button text verification failed!");
+        }
     }
 }
