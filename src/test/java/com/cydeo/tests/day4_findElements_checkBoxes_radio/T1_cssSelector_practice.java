f0bda9406587790bb5f273918b70987b9f7bc2e6
gabs1311 <gabs.antonovici@gmail.com>
2022-05-20 15:37:50 +0300

    day4 T1 Xpath and css selector

---
+++ src/test/java/com/cydeo/tests/day4_findElements_checkBoxes_radio/T1_cssSelector_practice.java
@@ -0,0 +1,46 @@
+package com.cydeo.tests.day4_findElements_checkBoxes_radio;
+
+public class T1_cssSelector_practice {
+    public static void main(String[] args) {
+        /*
+        XPATH and CSS Selector PRACTICES
+DO NOT USE ANY DEVELOPER TOOLS TO GET ANY LOCATORS.
+TC #1: XPATH and cssSelector Practices
+1. Open Chrome browser
+2. Go to https://practice.cydeo.com/forgot_password
+3. Locate all the WebElements on the page using XPATH and/or CSS
+locator only (total of 6)
+a. “Home” link
+b. “Forgot password” header
+c. “E-mail” text
+d. E-mail input box
+e. “Retrieve password” button
+f. “Powered by Cydeo text
+4. Verify all web elements are displayed.
+First solve the task with using cssSelector only. Try to create 2 different
+cssSelector if possible
+Then solve the task using XPATH only. Try to create 2 different
+XPATH locator if possible
+         */
+
+
+        //XPATH and CSS Selector PRACTICES
+        //DO NOT USE ANY DEVELOPER TOOLS TO GET ANY LOCATORS.
+        //TC #1: XPATH and cssSelector Practices
+        //1. Open Chrome browser
+        //2. Go to https://practice.cydeo.com/forgot_password
+        //3. Locate all the WebElements on the page using XPATH and/or CSS
+        //locator only (total of 6)
+        //a. “Home” link
+        //b. “Forgot password” header
+        //c. “E-mail” text
+        //d. E-mail input box
+        //e. “Retrieve password” button
+        //f. “Powered by Cydeo text
+        //4. Verify all web elements are displayed.
+        //First solve the task with using cssSelector only. Try to create 2 different
+        //cssSelector if possible
+        //Then solve the task using XPATH only. Try to create 2 different
+        //XPATH locator if possible
+    }
+}
