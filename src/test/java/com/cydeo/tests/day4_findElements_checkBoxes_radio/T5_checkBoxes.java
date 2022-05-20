c886d26806e3b12068362bbd52837e17a556c5bd
gabs1311 <gabs.antonovici@gmail.com>
2022-05-20 22:55:54 +0300

    day4 T5 check boxes

--- src/test/java/com/cydeo/tests/day4_findElements_checkBoxes_radio/T5_checkBoxes.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkBoxes_radio/T5_checkBoxes.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import java.util.concurrent.TimeUnit;
 
 public class T5_checkBoxes {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         /*Practice: Checkboxes
 1. Go to http://practice.cydeo.com/checkboxes
 2. Confirm checkbox #1 is NOT selected by default
@@ -36,7 +36,11 @@ public class T5_checkBoxes {
         //3. Confirm checkbox #2 is SELECTED by default.
         System.out.println("checkBox2.isSelected(), expected = true " + checkBox2.isSelected());
         //4. Click checkbox #1 to select it.
+        Thread.sleep(2000);
+        checkBox1.click();
         //5. Click checkbox #2 to deselect it.
+        Thread.sleep(2000);
+        checkBox2.click();
         //6. Confirm checkbox #1 is SELECTED.
         //7. Confirm checkbox #2 is NOT selected.
     }
