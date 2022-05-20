bc9061e096163168b1bc3acc1b958670f94d22c7
gabs1311 <gabs.antonovici@gmail.com>
2022-05-20 15:46:22 +0300

    day4 T1 Xpath and css selector

--- src/test/java/com/cydeo/tests/day4_findElements_checkBoxes_radio/T1_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkBoxes_radio/T1_cssSelector_practice.java
@@ -1,5 +1,8 @@
 package com.cydeo.tests.day4_findElements_checkBoxes_radio;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
 public class T1_cssSelector_practice {
     public static void main(String[] args) {
         /*
@@ -28,8 +31,15 @@ XPATH locator if possible
         //DO NOT USE ANY DEVELOPER TOOLS TO GET ANY LOCATORS.
         //TC #1: XPATH and cssSelector Practices
         //1. Open Chrome browser
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+
         //2. Go to https://practice.cydeo.com/forgot_password
+        driver.get("https://practice.cydeo.com/forgot_password");
+
         //3. Locate all the WebElements on the page using XPATH and/or CSS
+
         //locator only (total of 6)
         //a. “Home” link
         //b. “Forgot password” header
