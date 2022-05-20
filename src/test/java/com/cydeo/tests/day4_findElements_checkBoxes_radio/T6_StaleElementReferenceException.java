a7618cb65039c5e40a73fd0fdb139fcbd01d0320
gabs1311 <gabs.antonovici@gmail.com>
2022-05-20 23:25:25 +0300

    day4 T6 StaleElementReferenceException

---
+++ src/test/java/com/cydeo/tests/day4_findElements_checkBoxes_radio/T6_StaleElementReferenceException.java
@@ -0,0 +1,26 @@
+package com.cydeo.tests.day4_findElements_checkBoxes_radio;
+
+public class T6_StaleElementReferenceException {
+    public static void main(String[] args) {
+
+        /*TC #6: StaleElementReferenceException Task
+1- Open a chrome browser
+2- Go to: https://practice.cydeo.com/abtest
+3- Locate “CYDEO” link, verify it is displayed.
+4- Refresh the page.
+5- Verify it is displayed, again.
+This is a simple StaleElementReferenceException to understand what is
+this exception and how to handle it.
+
+         */
+
+        //TC #6: StaleElementReferenceException Task
+        //1- Open a chrome browser
+        //2- Go to: https://practice.cydeo.com/abtest
+        //3- Locate “CYDEO” link, verify it is displayed.
+        //4- Refresh the page.
+        //5- Verify it is displayed, again.
+        //This is a simple StaleElementReferenceException to understand what is
+        //this exception and how to handle it.
+    }
+}
