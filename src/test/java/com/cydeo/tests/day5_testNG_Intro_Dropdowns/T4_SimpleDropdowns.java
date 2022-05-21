09d6db67d4b64a30fc1537d2994c26dc2b9ebd71
gabs1311 <gabs.antonovici@gmail.com>
2022-05-21 20:45:07 +0300

    T4 Simple dropdowns

--- src/test/java/com/cydeo/tests/day5_testNG_Intro_Dropdowns/T4_SimpleDropdowns.java
+++ src/test/java/com/cydeo/tests/day5_testNG_Intro_Dropdowns/T4_SimpleDropdowns.java
@@ -1,7 +1,11 @@
 package com.cydeo.tests.day5_testNG_Intro_Dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -19,13 +23,13 @@ Expected: “Please select an option”
 4. Verify “State selection” default selected value is correct
 Expected: “Select a State”
      */
-
+WebDriver driver;
    @BeforeMethod
     public void SetupMethod() {
 
         //1. Open Chrome browser
         //2. Go to https://practice.cydeo.com/dropdown
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+       driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get(" https://practice.cydeo.com/dropdown");
@@ -36,7 +40,13 @@ Expected: “Select a State”
     public void SimpleDropdownTest() {
 
 
-        //3. Verify “Simple dropdown” default selected value is correct
+        //3. Verify “Simple dropdown” default selected value is correct;
+        Select simpleDropdown = new Select(driver.findElement(By.xpath("//select[@id='dropdown']")));
+        WebElement curentlySelectedOption = simpleDropdown.getFirstSelectedOption();
+        String actualSimpleDropdownText = curentlySelectedOption.getText();
+        String expectedSimpleDropdownText = "Please select an option";
+        Assert.assertEquals(actualSimpleDropdownText,expectedSimpleDropdownText);
+
         //Expected: “Please select an option”
         //4. Verify “State selection” default selected value is correct
         //Expected: “Select a State”
