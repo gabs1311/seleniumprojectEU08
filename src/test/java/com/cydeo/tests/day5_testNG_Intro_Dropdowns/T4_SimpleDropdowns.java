d5380dc4957ed1dfcc0e9f27b89bbb31def3ac78
gabs1311 <gabs.antonovici@gmail.com>
2022-05-21 20:17:18 +0300

    T4 Simple dropdowns

--- src/test/java/com/cydeo/tests/day5_testNG_Intro_Dropdowns/T4_SimpleDropdowns.java
+++ src/test/java/com/cydeo/tests/day5_testNG_Intro_Dropdowns/T4_SimpleDropdowns.java
@@ -1,5 +1,12 @@
 package com.cydeo.tests.day5_testNG_Intro_Dropdowns;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class T4_SimpleDropdowns {
 
     /*
@@ -13,12 +20,25 @@ Expected: “Please select an option”
 Expected: “Select a State”
      */
 
-    //TC#4: Verifying “Simple dropdown” and “State selection” dropdown
-    //default values
-    //1. Open Chrome browser
-    //2. Go to https://practice.cydeo.com/dropdown
-    //3. Verify “Simple dropdown” default selected value is correct
-    //Expected: “Please select an option”
-    //4. Verify “State selection” default selected value is correct
-    //Expected: “Select a State”
+   @BeforeMethod
+    public void SetupMethod() {
+
+        //1. Open Chrome browser
+        //2. Go to https://practice.cydeo.com/dropdown
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(" https://practice.cydeo.com/dropdown");
+    }
+
+
+    @Test
+    public void SimpleDropdownTest() {
+
+
+        //3. Verify “Simple dropdown” default selected value is correct
+        //Expected: “Please select an option”
+        //4. Verify “State selection” default selected value is correct
+        //Expected: “Select a State”
+    }
 }
