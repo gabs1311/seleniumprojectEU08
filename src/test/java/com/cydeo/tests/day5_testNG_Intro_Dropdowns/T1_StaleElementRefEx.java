ed399a5de13dee30991a79e2ef9a9dce80210bd3
gabs1311 <gabs.antonovici@gmail.com>
2022-05-21 13:10:11 +0300

    day5 T1 Stale Element reference exception practice

--- src/test/java/com/cydeo/tests/day5_testNG_Intro_Dropdowns/T1_StaleElementRefEx.java
+++ src/test/java/com/cydeo/tests/day5_testNG_Intro_Dropdowns/T1_StaleElementRefEx.java
@@ -1,5 +1,10 @@
 package com.cydeo.tests.day5_testNG_Intro_Dropdowns;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class T1_StaleElementRefEx {
     public static void main(String[] args) {
         /*XPATH PRACTICES
@@ -18,8 +23,13 @@ USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
         //DO NOT USE ANY DEVELOPER TOOLS TO GET ANY LOCATORS.
         //TC #1: StaleElementReferenceException handling
         //1. Open Chrome browser
+        WebDriver driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         //2. Go to https://practice.cydeo.com/add_remove_elements/
+        driver.get("https://practice.cydeo.com/add_remove_elements");
         //3. Click to “Add Element” button
+
         //4. Verify “Delete” button is displayed after clicking.
         //5. Click to “Delete” button.
         //6. Verify “Delete” button is NOT displayed after clicking.
