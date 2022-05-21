b3aebcaff56781c33be296134223b32b49e99a4a
gabs1311 <gabs.antonovici@gmail.com>
2022-05-21 13:03:37 +0300

    day5 T1 Stale Element

---
+++ src/test/java/com/cydeo/tests/day5_testNG_Intro_Dropdowns/T1_StaleElementRefEx.java
@@ -0,0 +1,28 @@
+package com.cydeo.tests.day5_testNG_Intro_Dropdowns;
+
+public class T1_StaleElementRefEx {
+    public static void main(String[] args) {
+        /*XPATH PRACTICES
+DO NOT USE ANY DEVELOPER TOOLS TO GET ANY LOCATORS.
+TC #1: StaleElementReferenceException handling
+1. Open Chrome browser
+2. Go to https://practice.cydeo.com/add_remove_elements/
+3. Click to “Add Element” button
+4. Verify “Delete” button is displayed after clicking.
+5. Click to “Delete” button.
+6. Verify “Delete” button is NOT displayed after clicking.
+USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
+
+         */
+        //XPATH PRACTICES
+        //DO NOT USE ANY DEVELOPER TOOLS TO GET ANY LOCATORS.
+        //TC #1: StaleElementReferenceException handling
+        //1. Open Chrome browser
+        //2. Go to https://practice.cydeo.com/add_remove_elements/
+        //3. Click to “Add Element” button
+        //4. Verify “Delete” button is displayed after clicking.
+        //5. Click to “Delete” button.
+        //6. Verify “Delete” button is NOT displayed after clicking.
+        //USE XPATH LOCATOR FOR ALL WEBELEMENT LOCATORS
+    }
+}
