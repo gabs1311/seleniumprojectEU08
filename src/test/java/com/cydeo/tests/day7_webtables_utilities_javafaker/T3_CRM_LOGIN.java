dc01be576e40f21b467db3bbd1200a5bf2bc85fe
gabs1311 <gabs.antonovici@gmail.com>
2022-06-17 14:02:16 +0300

    D7-T3- CRM

---
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -0,0 +1,36 @@
+package com.cydeo.tests.day7_webtables_utilities_javafaker;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T3_CRM_LOGIN {
+
+        public WebDriver driver;
+    @BeforeMethod
+    public void setupMethod(){
+    driver= WebDriverFactory.getDriver("chrome");
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+@Test
+    public void crm_login_test() {
+
+
+
+    //2. Go to : https://login1.nextbasecrm.com/
+
+    driver.get("https://login1.nextbasecrm.com/  ");
+    //3. Enter valid username
+    //4. Enter valid password
+    //5. Click to `Log In` button
+    //6. Verify title is as expected:
+
+}
+
+
+}
