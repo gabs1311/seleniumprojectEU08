0345e5b39357d324402b398453540301a723e497
gabs1311 <gabs.antonovici@gmail.com>
2022-06-18 13:09:27 +0300

    Day8

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/LearningProperties.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/LearningProperties.java
@@ -7,7 +7,11 @@ public class LearningProperties {
 
     @Test
     public void java_properties_reading_test(){
+        // key= value
+        //os.name=Mac OS X
+        // user.name= cyberteckchicago-1
 
         System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+        System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
     }
 }
---
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/ReadingProperties.java
@@ -0,0 +1,20 @@
+package com.cydeo.tests.day8_properties_config_reader;
+
+import org.testng.annotations.Test;
+
+public class ReadingProperties {
+    @Test
+
+    public void reading_from_properties_test(){
+
+
+
+
+
+
+    }
+
+
+
+
+}
