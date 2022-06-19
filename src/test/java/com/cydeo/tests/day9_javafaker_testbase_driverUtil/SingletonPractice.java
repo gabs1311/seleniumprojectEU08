736b9e7d8eabc48c6cfd1be69d8375cebe5139cd
gabs1311 <gabs.antonovici@gmail.com>
2022-06-19 10:47:19 +0300

    day9

---
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/SingletonPractice.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
+
+import org.testng.annotations.Test;
+
+public class SingletonPractice {
+
+    @Test
+    public void singleton_understand_test1(){
+
+
+    }
+}
