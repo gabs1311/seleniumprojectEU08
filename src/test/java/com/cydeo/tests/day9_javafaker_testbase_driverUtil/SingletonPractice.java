85e86553764f823eeb0220f698e14068c1093e4f
gabs1311 <gabs.antonovici@gmail.com>
2022-06-19 10:54:00 +0300

    day9

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/SingletonPractice.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/SingletonPractice.java
@@ -7,6 +7,18 @@ public class SingletonPractice {
     @Test
     public void singleton_understand_test1(){
 
+        String str1 = Singleton.getWord();
+        System.out.println("str1 = " + str1);
 
+        String str2 = Singleton.getWord();
+        System.out.println("str1 = " + str2);
+
+        String str3 = Singleton.getWord();
+        System.out.println("str1 = " + str3);
+    }
+    @Test
+    public void singleton_understanding_test2(){
+        String str4 = Singleton.getWord();
+        System.out.println("str1 = " + str4);
     }
 }
