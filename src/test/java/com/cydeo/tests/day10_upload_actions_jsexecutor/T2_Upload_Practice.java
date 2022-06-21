5063117d44dc52b517c92a30da2f1e867303b0aa
gabs1311 <gabs.antonovici@gmail.com>
2022-06-21 15:27:45 +0300

    day10- T2 - Upload-Practice

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.utilities.Driver;
 import org.testng.annotations.Test;
 
 public class T2_Upload_Practice {
@@ -8,6 +9,9 @@ public class T2_Upload_Practice {
     public void upload_test(){
         //TC #2 Upload Test
         //1. Go to https://practice.cydeo.com/upload
+        Driver.getDriver().get("https://practice.cydeo.com/upload");
+
+
         //2. Find some small file from your computer, and get the path of it.
         //3. Upload the file.
         //4. Assert:
