49ccb6c84055889cc29bc4bb78971720aea1e837
gabs1311 <gabs.antonovici@gmail.com>
2022-06-21 22:37:35 +0300

    day10- T2 - Upload-Practice

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
@@ -4,6 +4,7 @@ import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T2_Upload_Practice {
@@ -30,5 +31,7 @@ public class T2_Upload_Practice {
 
         //4. Assert:
         //-File uploaded text is displayed on the page
+        WebElement fileUploadedHeader=Driver.getDriver().findElement(By.tagName("h3"));
+        Assert.assertTrue(fileUploadedHeader.isDisplayed());
     }
 }
