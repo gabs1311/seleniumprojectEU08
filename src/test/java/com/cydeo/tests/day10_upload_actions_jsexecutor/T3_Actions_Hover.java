18b8cd26e68984b8202e698eb1450fb426e4f88b
gabs1311 <gabs.antonovici@gmail.com>
2022-06-22 16:26:36 +0300

    day10- T3- Actions Hover

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T3_Actions_Hover.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T3_Actions_Hover.java
@@ -1,6 +1,8 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class T3_Actions_Hover {
@@ -13,6 +15,17 @@ public class T3_Actions_Hover {
         //1. Go to https://practice.cydeo.com/hovers
 
             Driver.getDriver().get("https://practice.cydeo.com/hovers");
+
+            // Locate all images under here: -  at this point I am just locating them
+            WebElement image1 = Driver.getDriver().findElement(By.xpath("(//img)[1]"));
+            WebElement image2 = Driver.getDriver().findElement(By.xpath("(//img)[2]"));
+            WebElement image3 = Driver.getDriver().findElement(By.xpath("(//img)[3]"));
+
+            // Locate all the " user" texts under here -at this point I am just locating them
+
+            WebElement user1 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user1']"));
+            WebElement user2 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user2']"));
+            WebElement user3 = Driver.getDriver().findElement(By.xpath("//h5[text()='name: user3']"));
 
         //2. Hover over to first image
         //3. Assert:
