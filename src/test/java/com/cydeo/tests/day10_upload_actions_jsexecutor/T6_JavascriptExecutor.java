d05e886599bbcc6ec66b9e156df9566ba5a859ab
gabs1311 <gabs.antonovici@gmail.com>
2022-06-22 17:16:40 +0300

    day10- T6 -javascript_executor_test1

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavascriptExecutor.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavascriptExecutor.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.utilities.Driver;
 import org.testng.annotations.Test;
 
 public class T6_JavascriptExecutor {
@@ -8,6 +9,8 @@ public class T6_JavascriptExecutor {
        //TC #6: Scroll using JavascriptExecutor
         //1- Open a chrome browser
         //2- Go to: https://practice.cydeo.com/infinite_scroll
+
+        Driver.getDriver().get(" https://practice.cydeo.com/infinite_scroll");
         //3- Use below JavaScript method and scroll
         //a. 750 pixels down 10 times.
         //b. 750 pixels up 10 times
