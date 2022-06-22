ace293095c30225b684a89c79c75e96e35a1d40a
gabs1311 <gabs.antonovici@gmail.com>
2022-06-22 15:13:44 +0300

    day10- T2 - Upload-Practice

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T2_Upload_Practice.java
@@ -13,12 +13,24 @@ public class T2_Upload_Practice {
     public void upload_test(){
         //TC #2 Upload Test
         //1. Go to https://practice.cydeo.com/upload
-        Driver.getDriver().get("https://practice.cydeo.com/upload");
+        Driver.getDriver().get("https://practice.cydeo.com/upload");// we are getting the page
 
 
         //2. Find some small file from your computer, and get the path of it.
 
-        String path = "D:\\Cybertech school\\Project\\sprint 2\\rsz_british_short_hair.jpg";// located the file from the computer
+        /*  these are the necessary steps
+        Windows:
+#1- Right click on the file
+#2- Select "properties"
+#3- Go to "Security" tab
+#4- Get the path from "Object name"
+For windows, either double backward "\\" or single forward "/" works.
+String path ="C:\\Users\\hayat\\Desktop\\note.txt
+
+         */
+
+        String path = "D:\\Cybertech school\\Project\\sprint 2\\rsz_british_short_hair.jpg";// located the file
+        // from the computer, passing the path for the file
 
         //3. Upload the file.
         WebElement fileUpload = Driver.getDriver().findElement(By.id("file-upload"));// uploaded the element
