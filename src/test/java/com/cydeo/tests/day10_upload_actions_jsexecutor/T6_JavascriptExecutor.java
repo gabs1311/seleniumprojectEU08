449a645c29e29c4864864ea7ab3bb5c04494558d
gabs1311 <gabs.antonovici@gmail.com>
2022-06-22 17:39:47 +0300

    day10- T6 -javascript_executor_test1

--- src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavascriptExecutor.java
+++ src/test/java/com/cydeo/tests/day10_upload_actions_jsexecutor/T6_JavascriptExecutor.java
@@ -1,6 +1,8 @@
 package com.cydeo.tests.day10_upload_actions_jsexecutor;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 
 public class T6_JavascriptExecutor {
@@ -11,9 +13,28 @@ public class T6_JavascriptExecutor {
         //2- Go to: https://practice.cydeo.com/infinite_scroll
 
         Driver.getDriver().get(" https://practice.cydeo.com/infinite_scroll");
+
+      //  ((JavascriptExecutor)Driver.getDriver()).executeScript(""); // this is another option
+        //  to reach the link. Since JavaScript is different level, I have to downcast it to use it
+
         //3- Use below JavaScript method and scroll
-        //a. 750 pixels down 10 times.
+
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+            //a. 750 pixels down 10 times.
+        for (int i = 0; i <10 ; i++) {
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0,750)");
+        }
+
+
+
         //b. 750 pixels up 10 times
+
+        for (int i = 0; i <10 ; i++) {
+            BrowserUtils.sleep(1);
+            js.executeScript("window.scrollBy(0,-750)");
+        }
+
         //JavaScript method to use : window.scrollBy(0,0)
     }
 }
