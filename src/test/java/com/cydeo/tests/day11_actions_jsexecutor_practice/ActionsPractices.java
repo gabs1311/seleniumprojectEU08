bddf6061d8e65173ed7361afe98624f17f628fd7
gabs1311 <gabs.antonovici@gmail.com>
2022-06-22 21:22:56 +0300

    day11- ActionsPractices

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractices.java
@@ -2,6 +2,7 @@ package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
@@ -30,6 +31,8 @@ public class ActionsPractices {
         //TC #5: Scroll practice 2
         //1- Continue from where the Task 4 is left in the same test.
         //2- Scroll back up to “Home” link using PageUP button
+        actions.sendKeys(Keys.PAGE_UP, Keys.PAGE_UP).perform();// it is not for sure it will go
+        // all the way up to the link, so I use Keys.PAGE_UP twice, but it looks it is not enough
 
 
     }
