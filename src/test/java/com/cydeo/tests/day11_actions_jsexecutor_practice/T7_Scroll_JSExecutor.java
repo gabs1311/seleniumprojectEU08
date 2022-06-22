e6eea93d0644a1e3ebd78dbbbac5fc8b27238f89
gabs1311 <gabs.antonovici@gmail.com>
2022-06-22 22:53:07 +0300

    day11-T7_Scroll_JSExecutor

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T7_Scroll_JSExecutor.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T7_Scroll_JSExecutor.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
@@ -14,10 +16,30 @@ public class T7_Scroll_JSExecutor {
     //1- Open a chrome browser
     //2- Go to: https://practice.cydeo.com/large
     Driver.getDriver().get(" https://practice.cydeo.com/large");
-    //3- Scroll down to “Cydeo” link
+
+
     WebElement cydeoLink= Driver.getDriver().findElement(By.linkText("CYDEO"));
-    //4- Scroll up to “Home” link
     WebElement homeLink = Driver.getDriver().findElement(By.linkText("Home"));
+    // Downcasting our driver type to JavascriptExecutor, so we are able to use the methods that come from that interface
+    JavascriptExecutor js= (JavascriptExecutor) Driver.getDriver();
+
+    js.executeScript("arguments[0].scrollIntoView(true)", cydeoLink);// cydeoLink is index nr 0
+
+    //3- Scroll down to “Cydeo” link
+    BrowserUtils.sleep(2);// not necessary, it is placed here just for educational purpose,
+    // to be able to see the movement. But in real coding it is not to be placed
+    js.executeScript("arguments[0].scrollIntoView(true)", cydeoLink);
+
+    //4- Scroll up to “Home” link
+    BrowserUtils.sleep(2);
+  //  js.executeScript("arguments[1].scrollIntoView(true)", cydeoLink, homeLink);// cydeoLink is index 0, homeLink is index 1
+
+    js.executeScript("arguments[0].scrollIntoView(true)",  homeLink);
+
+    //objects
+    //cydeoLink          0
+    //homeLink           1
+    //someOtherLink      2
 
     //5- Use below provided JS method only
     //JavaScript method to use : arguments[0].scrollIntoView(true)
