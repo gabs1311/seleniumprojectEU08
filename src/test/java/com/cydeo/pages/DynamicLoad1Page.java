01c1a26baa7ccacb2ef6cf1b1157ee9cc234aab1
gabs1311 <gabs.antonovici@gmail.com>
2022-06-24 14:41:08 +0300

    pages

--- src/test/java/com/cydeo/pages/DynamicLoad1Page.java
+++ src/test/java/com/cydeo/pages/DynamicLoad1Page.java
@@ -1,6 +1,8 @@
 package com.cydeo.pages;
 
 import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DynamicLoad1Page {
@@ -30,8 +32,25 @@ public class DynamicLoad1Page {
 
     public DynamicLoad1Page(){
         PageFactory.initElements(Driver.getDriver(),this);
-
-
-
     }
+
+
+    @FindBy(xpath = "//button[.='Start']")
+    public WebElement startButton;
+
+    @FindBy(css = "div#loading")
+    public WebElement loadingBar;
+
+    @FindBy(css = "div#username")
+    public WebElement userName;
+
+    @FindBy(css = "#pwd")
+    public WebElement password;
+
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement submitButton;
+
+    @FindBy(id="flash")
+    public WebElement errorMessage;
 }
